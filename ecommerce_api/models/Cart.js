abfe1b783ab4e40e20b9bb35af402c4698321e74
iomckn <156865257+iomckn@users.noreply.github.com>
2025-02-11 15:09:59 +0100

    Delete ecommerce_api directory

--- ecommerce_api/models/Cart.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cartSchema = new mongoose.Schema({
-    userId: String,
-    products: [{ productId: String, quantity: Number }]
-});
-
-module.exports = mongoose.model('Cart', cartSchema);
